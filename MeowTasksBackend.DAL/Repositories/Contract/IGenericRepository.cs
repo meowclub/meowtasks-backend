3365f898c8ae2d9b2014e3d405dff043faa02a28
alemndev <140274341+alemndev@users.noreply.github.com>
2024-05-13 14:27:37 -0500

    feat(dal): implemented Generic repository

---
+++ MeowTasksBackend.DAL/Repositories/Contract/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace MeowTasksBackend.DAL.Repositories.Contract
+{
+  public interface IGenericRepository<T> where T : class
+  {
+    T? Get(Expression<Func<T, bool>> filter);
+    Task<T> Create(T model);
+    Task<bool> Update(T model);
+    Task<bool> Delete(T model);
+    IQueryable<T> Query(Expression<Func<T,  bool>> filter);
+  }
+}
---
+++ MeowTasksBackend.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using MeowTasksBackend.DAL.DBContext;
+using MeowTasksBackend.DAL.Repositories.Contract;
+
+namespace MeowTasksBackend.DAL.Repositories
+{
+  public class GenericRepository<T> : IGenericRepository<T> where T : class
+  {
+    private readonly MeowtasksdbContext _mwtdbContext;
+
+    public GenericRepository(MeowtasksdbContext mwtdbContext)
+    {
+      _mwtdbContext = mwtdbContext;
+    }
+
+    public async Task<T> Create(T model)
+    {
+      try
+      {
+        _mwtdbContext.Set<T>().Add(model);
+        await _mwtdbContext.SaveChangesAsync();
+
+        return model;
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+
+    public async Task<bool> Delete(T model)
+    {
+      try
+      {
+        _mwtdbContext.Set<T>().Remove(model);
+        await _mwtdbContext.SaveChangesAsync();
+
+        return true;
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+
+    public T? Get(Expression<Func<T, bool>> filter)
+    {
+      try
+      {
+        return _mwtdbContext.Set<T>().FirstOrDefault(filter);
+      }
+      catch (Exception)
+      {
+
+        throw;
+      }
+    }
+
+    public IQueryable<T> Query(Expression<Func<T, bool>> filter)
+    {
+      try
+      {
+        var query = filter == null ? _mwtdbContext.Set<T>() : _mwtdbContext.Set<T>().Where(filter);
+
+        return query;
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+
+    public async Task<bool> Update(T model)
+    {
+      try
+      {
+        _mwtdbContext.Set<T>().Update(model);
+        await _mwtdbContext.SaveChangesAsync();
+
+        return true;
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+  }
+}
